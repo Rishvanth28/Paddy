8afcfba936f9c4a0d1265e4126dda044493825da
adithya14255 <adithya14255@gmail.com>
2025-04-14 00:07:01 +0530

    merge conflict

--- paddy_proj/paddy_app/views.py
+++ paddy_proj/paddy_app/views.py
@@ -8,7 +8,8 @@ from datetime import date
 from django.core.paginator import Paginator
 from django.utils import timezone
 from .decorators import role_required
-from .helpers import *import json
+from .helpers import *
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 
 
