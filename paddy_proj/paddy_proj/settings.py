0dce50a18deab0ed5879b634c88f2ae3f7c2f10c
athithya99 <athithya@pinesphere.com>
2025-10-30 13:00:07 +0530

    Update settings.py

--- paddy_proj/paddy_proj/settings.py
+++ paddy_proj/paddy_proj/settings.py
@@ -139,6 +139,8 @@ import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "paddy_app/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # ✅
+
 
 
 MEDIA_URL = '/media/'
